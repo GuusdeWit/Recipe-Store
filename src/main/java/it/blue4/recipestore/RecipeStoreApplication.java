76e961b4e0737444b1f35131ba4c49a2ef35aa9d
guusdewit <g.dewit@quin.md>
2022-07-24 21:02:32 +0200

    project setup

---
+++ src/main/java/it/blue4/recipestore/RecipeStoreApplication.java
@@ -0,0 +1,13 @@
+package it.blue4.recipestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeStoreApplication.class, args);
+	}
+
+}
